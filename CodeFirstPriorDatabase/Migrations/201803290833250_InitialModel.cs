330ca773a8e5710024703ca393808948c31b1f6c
JohnMorris4 <johnmorris@morrisje.com>
2018-03-29 04:39:00 -0400

    Initial Setup

---
+++ CodeFirstPriorDatabase/Migrations/201803290833250_InitialModel.cs
@@ -0,0 +1,16 @@
+namespace CodeFirstPriorDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
